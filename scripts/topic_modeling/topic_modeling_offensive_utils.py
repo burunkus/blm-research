f4b9c5ad8cef113b0edd3e8216c625d0d25c8376
Ebuka Okpala <31814903+burunkus@users.noreply.github.com>
2024-07-29 12:46:46 -0400

    Delete scripts directory

--- scripts/topic_modeling/topic_modeling_offensive_utils.py
+++
@@ -1,6 +0,0 @@
-CURRENT_YEAR = '2021'
-CURRENT_EXPERIMENT_CLASS = 'offensive'
-SAVE_MODEL_PATH = "../../../../../zfs/socbd/eokpala/blm_research/models/topic_models/"
-SAVE_DOCUMENT_INFO_PATH = "../../../../../zfs/socbd/eokpala/blm_research/data/topic_modeling_data/document_info/"
-
-OFFENSIVE_PATH = "../../../../../zfs/socbd/eokpala/blm_research/data/topic_modeling_data/unique_tweets_2021_preprocessed_with_sentiment_race_gender_emotion_and_label_offensive_compliant.csv"
